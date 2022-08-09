bd0e204d6d0b16357bfcbc32e48e7d60527c45d0
Ramiro Conde <ramaconde85@gmail.com>
2022-08-09 10:17:56 -0300

    version 0.2

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -3,22 +3,26 @@
 
 function convertir(){
  
-  let DolarOficial= "140"
-  let DolarBlue="284"
-  let euro="135"
+  const precios= [140, 290, 135]
+  precios.push (30)
+  
   let resultado="0"
   let valor =parseInt(document.getElementById("valor").value)
 
   if(document.getElementById("uno").checked){
-    resultado= valor/DolarOficial;
+    resultado= valor/precios[0];
     alert("El cambio de pesos a dolar es:" + resultado)
 
   }else if(document.getElementById("dos").checked){
-    resultado= valor /DolarBlue;
+    resultado= valor /precios[1];
     alert("El cambio de pesos a dolar blue es:" + resultado)
 }else if (document.getElementById("tres").checked) {
- resultado= valor/euro;
+ resultado= valor/precios[2];
  alert("El cambio de pesos a euros es:" + resultado)
-
-} else {alert("No se ingreso ningun dato")}
+ 
+}  else if (document.getElementById("cuatro").checked) {
+  resultado= valor/precios[3];
+  alert("El cambio de pesos a real es:" + resultado)
+  
+} else (alert("No se ingreso ningun dato"))
 }
